291fda4f1d7482e08ba4b94486b80899d53bdbce
Michael Gaeta <michael.gaeta@af83.com>
2015-02-11 16:42:44 +0100

    refactor menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,18 +8,20 @@
   Menu.prototype = {
 
     create: function () {
+      this.addText();
+    },
+
+    addText: function () {
       this.startText = this.add.text(
-          this.game.width / 2,
-          this.game.height / 2,
-          " Shoot Game \n START ",
-          {font: "24px Arial", fill: '#123456', align: 'center' } // style du texte
-        );
+        this.game.width / 2,
+        this.game.height / 2,
+        " Shoot Balloon Game \n Click to START ",
+        {font: "36px Arial", fill: '#123456', align: 'center' } // style du texte
+      );
       this.startText.anchor.setTo(0.5, 0.5); // permet de centrer l'élément
       this.startText.inputEnabled = true; // autorise evenements
       this.startText.events.onInputDown.add(this.onDown, this);// ajout d'un evenement onmousedown pour demarrer le jeu
     },
-
-    update: function () {},
 
     onDown: function () {
       console.log("game > menu");
